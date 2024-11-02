b466c12c474430c7c5e623872cd7c689aae8ec4b
s33091MichalKaminski <michal.kaminski232@gmail.com>
2024-11-02 19:57:42 +0100

    Update 2_2.cpp

--- 2_2.cpp
+++ 2_2.cpp
@@ -1,46 +1,66 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{   
+int main() {   
     int a;
     int b;
 
-    cout << "podaj liczbe a" << endl;
+    cout << "Podaj liczbe a: ";
     cin >> a;
-    cout << "podaj liczbe b" << endl;
+    cout << "Podaj liczbe b: ";
     cin >> b;
 
-    cout << "wpisano liczbe a == " << a << " oraz b == " << b << endl;
-    cout << "wiersz gwiazdek o dlugosci a"<<endl;
+    cout << "Wpisano liczby a == " << a << " oraz b == " << b << endl;
+
+    cout << "\nWiersz gwiazdek o dlugosci a:" << endl;
     for (int i = 0; i < a; i++) {
         cout << "*";
     }
-    cout<<endl;
-    cout << "kolumna gwiazdek o dlugosci b" << endl;
+    cout << endl;
+
+    cout << "\nKolumna gwiazdek o dlugosci b:" << endl;
     for (int i = 0; i < b; i++) {
-        cout << "*"<<endl;
+        cout << "*" << endl;
     }
-    cout<<endl;
-    cout << "wypelniony gwiazdkami prostokat o wymiarach a na b " << endl;
-    for (int i = 0; i < b; i++){
+
+    cout << "\nWypelniony gwiazdkami prostokat o wymiarach a na b:" << endl;
+    for (int i = 0; i < b; i++) {
         for (int j = 0; j < a; j++) {
             cout << "*";
         }
         cout << endl;
     }
-    cout << endl;
-    cout << "obwod gwiazdkami prostokata o wymiarach a na b " << endl;
+
+    cout << "\nObwod gwiazdkami prostokata o wymiarach a na b:" << endl;
     for (int i = 0; i < b; i++) {
         for (int j = 0; j < a; j++) {
             if (i == 0 || i == b - 1 || j == 0 || j == a - 1) {
                 cout << "*";
-            }
-            else {
+            } else {
                 cout << " ";
             }
         }
         cout << endl;
     }
+
+    cout << "\nTrojkat prostokatny rownoramienny z katem prostym w lewym dolnym rogu:\n";
+    for (int i = 1; i <= a; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    cout << "\nTrojkat prostokatny rownoramienny z katem prostym w prawym gornym rogu:\n";
+    for (int i = 0; i < a; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << " ";
+        }
+        for (int k = i; k < a; k++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
